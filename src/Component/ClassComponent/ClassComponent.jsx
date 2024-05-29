c8506dc55152f8be0301d839d1779e7ad3cfd94a
Vishnu Kumar <vermaji1891@gmail.com>
2024-05-29 11:39:53 +0530

    added componentDidMount

--- src/Component/ClassComponent/ClassComponent.jsx
+++ src/Component/ClassComponent/ClassComponent.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-
-
 class Person extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            count: 0,
+            name: "Vishnu"
+        }
+        console.log("Inside Constructor")
+    }
 
+    componentDidMount() {
+        console.log("componentDidMount Called")
+    }
 
 
     render(){
+        console.log("Inside Render")
         return (
             <h1>I am Class Component</h1>
         )
--- src/Component/Functional/FunctionalComponent.jsx
+++ src/Component/Functional/FunctionalComponent.jsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react";
 
 
 const User = () => {
+    const [count, setCount] = useState(0);
 
-
+    useEffect (() => {
+        console.log("useEffect called");
+    }, [])
+    console.log("Inside functional Component!")
     return (
         <>
             <h1>I am functional Component!</h1>
