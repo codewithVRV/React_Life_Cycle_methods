7624b0d4598810abeb8cad9f717aacbca35947e5
Vishnu Kumar <vermaji1891@gmail.com>
2024-05-29 11:55:43 +0530

    added componentWllUnmount

--- src/Component/ClassComponent/ClassComponent.jsx
+++ src/Component/ClassComponent/ClassComponent.jsx
@@ -2,36 +2,20 @@ import React from "react";
 class Person extends React.Component {
     constructor (props) {
         super(props)
-        this.state = {
-            count: 0,
-            name: "Vishnu"
-        }
         console.log("Inside Constructor")
     }
 
-    componentDidMount() {
-        console.log("componentDidMount Called")
+
+    componentWillUnmount(){
+        console.log("componentWillUnmount Called")
     }
-
-    componentDidUpdate(preProps, preState) {
-        // console.log("preProps, preState", preProps, preState)
-        console.log("componentDidUpdate Called");
-    }
-
-
-
 
     render(){
         console.log("Inside Render")
-        const {count} = this.state;
         return (
             <>
                 <h1>I am Class Component</h1>
-                <button onClick={() => {
-                    this.setState({
-                        count: this.state.count+10
-                    })
-                }}>Click: {count}</button>
+                <button>Click</button>
             </>
         )
     }
--- src/Component/Functional/FunctionalComponent.jsx
+++ src/Component/Functional/FunctionalComponent.jsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 
 const User = () => {
-    const [count, setCount] = useState(0);
 
     useEffect (() => {
         console.log("useEffect called");
-    }, [count])
+        return () => {
+            console.log("CleanUp Function Called!")
+        }
+    }, [])
+
+
     console.log("Inside functional Component!")
     return (
         <>
             <h1>I am functional Component!</h1>
-            <button onClick={() => setCount(count+20)}>Click: {count}</button>
+            <button>Click</button>
         </>
     )
 }
